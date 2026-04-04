f35f6c8edef17fe414943f253784f43a4da64f38
subashpadaikathu <subashnaren82@gmail.com>
2026-04-04 16:14:05 +0530

    project8

---
+++ project8/script.js
@@ -0,0 +1,181 @@
+// let count = 1;
+// while(count < 100){
+//     if((count%3==0)&&(count%5==0)){
+//       console.log(count)
+//     }
+//     count++
+// }
+
+// let count = 1;
+// while(count < 100){
+//     if((count%3==0) || (count%5==0)){
+//       console.log(count)
+//     }
+//     count++
+// }
+
+
+// let givennumber = 10;
+// let i = 1;
+// while(i <= givennumber){
+//     if(givennumber%i==0){
+//       console.log(i)
+//     //   i++
+//     }
+//     i++
+    
+// }
+
+
+// let givennumber = 20;
+// let i = 1;
+// let count=0;
+// while(i <= givennumber){
+//     if(givennumber%i==0){
+//       console.log(i)
+//       count++
+      
+//     }
+//     i++
+
+// }
+// console.log(count)
+
+
+
+
+
+
+
+
+// let givennumber = 9;
+// let i = 1;
+// let count=0;
+
+// while (i<=givennumber){
+//     if(givennumber%i == 0){
+//         count++
+//     }
+//     i++
+// }
+
+// if(count == 2){
+//     console.log("prime number")
+// }else{
+//     console.log("not a prime number")
+// }
+
+
+// function main(){
+//    // பக்கம் 1
+//    move();
+//    turnLeft();
+   
+//    // பக்கம் 2
+//    move();
+//    turnLeft();
+   
+//    // பக்கம் 3
+//    move();
+//    turnLeft();
+   
+//    // பக்கம் 4 (இதை நீங்கள் சேர்க்க வேண்டும்)
+//    move();
+//    turnLeft();
+// }
+
+
+
+// let rev = 0;
+// let no = 1234567;
+// while(no>0){
+//     rev = (rev * 10) + no%10;
+//     no=Math.floor(no/10);
+// }
+// console.log(rev);
+
+
+
+
+// const mark = [0,50,50,50,50]
+// let fCount = 0;
+// let i = 0;
+// let sum = 0;
+// while(i<5){
+// sum = sum + mark[i];
+// if (mark[i]<50){
+//     fCount++
+    
+// }
+// i++;
+
+
+// }
+// let avg = sum/mark.length;
+
+// if (fCount>0){
+   
+// console.log("NO GRADE")
+
+
+// } else {
+//    console.log("failCount is ;",fCount);
+//    console.log("average mark is:",avg);
+//    console.log("total mark:",sum);
+
+// }
+
+ 
+//    if (avg>=90){
+//     console.log("A grade")
+//    } else if(avg>=80){
+//     console.log("B grade")
+//    } else if (avg>=70){
+//     console.log("C grade")
+//    }else if (avg>=50){
+//     console.log("D grade")
+//    } else {
+//     console.log("E grade")
+//    }
+
+
+
+
+
+
+
+
+
+// let subash = (n1,n3) => n1*n3;
+
+// console.log(subash(3,3));
+// console.log(subash(10,10))
+// console.log(subash(1,10))
+// console.log(subash(12,10))
+// console.log(subash(15,10))
+
+
+
+
+
+let subash = (n1,n3) => n1%n3;
+
+console.log(subash(3,3));
+console.log(subash(10,10))
+console.log(subash(1,10))
+console.log(subash(12,10))
+console.log(subash(15,10))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
