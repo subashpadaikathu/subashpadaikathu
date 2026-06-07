21293bc4185a7c680632e7bcc1d2aee3680d385f
subashpadaikathu <subashnaren82@gmail.com>
2026-06-07 18:11:07 +0530

    pythonwhilelooppractise

---
+++ pythonproject/practise.py
@@ -0,0 +1,112 @@
+# count = 0
+# while count < 5:
+#     print("1", end=" ")
+#     count+=1
+# count = 1
+# while count <= 5:
+#     print(count, end=" ")
+#     count+=1
+
+
+# count = 1
+# while count <= 9:
+#     print(count, end=" ")
+#     count+=2
+
+
+# count = 3
+# while count <= 15:
+#     print(count, end=" ")
+#     count+=3
+
+
+# count = 15
+# while count >= 3:
+#     print(count, end=" ")
+#     count-=3
+
+# count = 10
+# while count >= 2:
+#     print(count, end=" ")
+#     count-=2
+
+
+
+# count = 9
+# while count >= 1:
+#     print(count, end=" ")
+#     count-=2
+
+
+# count = 9
+# while count >= 1:
+#     print(count, end=" ")
+#     count-=2
+
+
+
+# count = 1
+# while count <= 100:
+#     if count % 3 == 0 and count % 5 == 0:
+#         print(count)
+#     count+=1    
+
+
+# count = 1
+# while count <= 100:
+#     if count % 3 == 0 or count % 5 == 0:
+#         print(count)
+#     count+=1    
+
+# givennumber = 15
+# count = 1
+# total = 0
+# while count <= givennumber:
+#     if givennumber % count == 0:
+#         print(count)
+#         total+=count
+#     count+=1    
+
+# print("total :",total)
+
+
+
+
+# givennumber = 15
+# count = 1
+# total = 0
+# while count <= givennumber/2:
+#     if givennumber % count == 0:
+#         print(count)
+#         total+=count
+#     count+=1    
+# if total == givennumber:
+#     print("perfect number because total and givennumber are same")
+# else:
+#      print(" not a perfect number,because total and givennumber are not same")
+
+
+
+
+# givennumber = 37
+
+# if givennumber < 2:
+#     print("not a prime number")
+# else:
+#     count = 2
+
+#     while count <= givennumber//2:
+#         if givennumber % count == 0:
+#             print("not a prime number")
+#             break
+#         count += 1
+#     else:
+#         print("prime number")
+
+
+
+
+
+
+            
+
